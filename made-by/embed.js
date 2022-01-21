37ab17a319085bd527a789939e54ffe3e63ec3a8
Jothin-kumar <contact@jothin.tech>
2022-01-21 23:37:44 +0530

    Decrease size.

--- made-by/embed.js
+++ made-by/embed.js
@@ -4,12 +4,14 @@ function displayMadeBy (bg, fg, shadow) {
     const subtitles = document.createElement('p');
     logo.src = 'https://jothin.tech/favicon.png';
     logo.alt = 'Jothin kumar';
-    logo.style.width = '25%';
-    logo.style.height = '25%';
+    logo.style.width = '20%';
+    logo.style.height = '20%';
     logo.style.borderRadius = '50%';
+    logo.style.padding = '1px';
     element.appendChild(logo);
-    subtitles.innerText = 'Website made by Jothin kumar';
+    subtitles.innerText = 'Website made by<br>Jothin kumar';
     subtitles.style.fontWeight = 'bolder';
+    subtitles.style.padding = '1px';
     element.appendChild(subtitles);
     element.style.position = 'fixed';
     element.style.bottom = '0';
@@ -19,10 +21,10 @@ function displayMadeBy (bg, fg, shadow) {
     element.style.backgroundColor = bg;
     element.style.color = fg;
     element.style.zIndex = '100';
-    element.style.fontSize = 'large';
+    element.style.fontSize = 'medium';
     element.style.boxShadow = '1px 1px 3px ' + shadow;
     element.style.textAlign = 'center';
-    element.style.maxWidth = '20%';
+    element.style.maxWidth = '10%';
     element.style.textDecoration = 'none';
     element.id = 'made-by-jothin-kumar-widget'
     element.href = 'https://jothin.tech' + '?utm_source=' + window.location.hostname + '&utm_medium=widget&utm_campaign=made-by-jothin-kumar-widget';
